767da3f6e7e5d2d23f0a688d59a1a8cada8355dd
yazuka000 <yazuka.000@gmail.com>
2022-08-30 11:10:44 +0900

    Add users table and user model in alpha-blog

--- alpha-blog/db/schema.rb
+++ alpha-blog/db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_06_080044) do
+ActiveRecord::Schema.define(version: 2022_08_30_020258) do
 
   create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "update_at"
+  end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
