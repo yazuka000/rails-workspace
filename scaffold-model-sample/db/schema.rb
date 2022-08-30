ddba660c72960cf06c737037361ce726c4a2cbcc
yazuka000 <yazuka.000@gmail.com>
2022-08-30 10:11:01 +0900

    Learn one-to-many associations with rails console

--- scaffold-model-sample/db/schema.rb
+++ scaffold-model-sample/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_29_001713) do
+ActiveRecord::Schema.define(version: 2022_08_30_004024) do
 
   create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
