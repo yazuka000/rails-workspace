8da68b0fbc66b74663052f04f5b51efb15b01d13
yazuka000 <yazuka.000@gmail.com>
2022-09-02 10:16:58 +0900

    Add link of user's profile and user's articles in alpha-blog

--- alpha-blog/app/helpers/application_helper.rb
+++ alpha-blog/app/helpers/application_helper.rb
@@ -10,4 +10,12 @@ module ApplicationHelper
     end
   end
 =end
+
+  def gravater_for(user, options = { size: 80 })
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    size = options[:size]
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
+  end
 end
